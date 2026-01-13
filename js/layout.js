61e290065bfc7318b493095d5bd18033736618aa
JakeJ16c <jakejones16c@gmail.com>
2026-01-13 13:49:10 +0000

    Create layout.js

---
+++ js/layout.js
@@ -0,0 +1,5 @@
+// js/layout.js
+const shell = document.getElementById("app-shell");
+shell.innerHTML = `<div style="padding:16px; font-family: Fredoka, system-ui;">
+  ✅ layout.js is loading
+</div>`;
